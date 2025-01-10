061c408f03884b6777e09fe47b57907b11e60ae6
totopero <138760537+totopero@users.noreply.github.com>
2025-01-11 04:08:15 +0900

    Update index.js

--- themes/movie/index.js
+++ themes/movie/index.js
@@ -101,7 +101,8 @@ const LayoutBase = props => {
             </div>
           </div>
         </div>
-
+        {/* 页脚 */}
+        <Footer {...props} />
         {/* 搜索框 */}
         <AlgoliaSearchModal cRef={searchModal} {...props} />
 
